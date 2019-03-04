ce3288c34962e4c7a42def5183db597eb920b09e
Peter Keating <peter@moov2.com>
2019-03-04 10:08:16 +0000

    Create Orchard Core theme via boilerplate template
    Used https://github.com/moov2/Moov2.OrchardCore.ThemeBoilerplate to
    create starting point for theme.

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Studio",
+    Author = "Moov2 Ltd.",
+    Website = "https://moov2.com",
+    Version = "0.0.1",
+    Description = "Theme for game studio website"
+)]
